f3dc68c790e65decf8914de36fb2b88aec9d32f2
Philip Alegret <philip.alegret@gmail.com>
2023-12-21 15:47:56 -0500

    Added web-client.

--- TrickingLibraryApi/Program.cs
+++ TrickingLibraryApi/Program.cs
@@ -7,6 +7,10 @@ public static class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var services = builder.Services;
+
+        services.AddControllers();
+        
         var app = builder.Build();
 
         if (app.Environment.IsDevelopment())
