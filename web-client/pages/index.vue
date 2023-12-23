7ebbf16cdc3e3868e9b675e4fac2d1872620f6f5
Philip Alegret <philip.alegret@gmail.com>
2023-12-23 09:39:56 -0500

    Update to build job name in build.yaml.

--- web-client/pages/index.vue
+++ web-client/pages/index.vue
@@ -1,147 +1,94 @@
 <!--suppress ALL-->
 
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo/>
-        <vuetify-logo/>
-      </div>
-
-      <v-card v-if="tricks">
-        <v-card-title>
-          <span class="headline">Tricks</span>
-        </v-card-title>
-        <v-card-text>
-          <ul>
-            <li v-for="trick in tricks" :key="trick.id">
-              {{trick.name}}
-            </li>
-          </ul>
-        </v-card-text>      
-      </v-card>
-      <v-divider class="my-3"></v-divider>
-      <v-card class="mb-3">
-        <v-card-title>
-          <span class="headline">Create a new trick</span>
-        </v-card-title>
-        <v-card-text>
-          <v-text-field 
-            label="Trick name" 
-            v-model="trickName" 
-            required 
-          />
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn color="primary" @click="saveTrick">
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          {{message}}
-          <v-btn @click="reset" class="ml-2">Reset</v-btn>
-          <v-btn @click="resetTricks" class="ml-1">Reset Tricks</v-btn>
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower
-            developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-            href="https://vuetifyjs.com"
-            target="_blank"
-          >
-            documentation
-          </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-            href="https://chat.vuetifyjs.com/"
-            target="_blank"
-            title="chat"
-          >
-            discord
-          </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-            href="https://github.com/vuetifyjs/vuetify/issues"
-            target="_blank"
-            title="contribute"
-          >
-            issue board
-          </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the
-            future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <v-card v-if="haveTricks">
+      <v-card-title>
+        <span class="headline">Tricks</span>
+      </v-card-title>
+      <v-card-text>
+        <ul>
+          <li v-for="trick in tricks" :key="trick.id">
+            {{trick.name}}
+          </li>
+        </ul>
+      </v-card-text>
+    </v-card>
+    <v-divider v-if="haveTricks" class="my-3"></v-divider>
+    <v-stepper v-model="step">
+      <v-stepper-header>
+        <v-stepper-step :complete="step > 1" step="1">
+          Upload Video
+        </v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step :complete="step > 2" step="2">
+          Trick Information
+        </v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step step="3">
+          Confirmation
+        </v-stepper-step>
+      </v-stepper-header>
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-card class="mb-12" height="200px">
+            <v-card-text>
+              <v-file-input accept="video/*" @change="handleFile"></v-file-input>
+            </v-card-text>
+          </v-card>
+        </v-stepper-content>
+        <v-stepper-content step="2">
+          <v-card class="mb-12" height="200px">
+            <v-card-text>
+              <v-text-field
+                label="Trick name"
+                v-model="trickName"
+                required
+              />
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer/>
+              <v-btn color="primary" @click="saveTrick">
+                Save Trick
+              </v-btn>
+              <v-btn @click="resetTricks" class="ml-1">
+                Reset Tricks
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-stepper-content>
+        <v-stepper-content step="3">
+          <v-card class="mb-12" height="200px">
+            <v-card-text>
+              <h1>Success!</h1>
+            </v-card-text>
+          </v-card>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Axios from 'axios';
 import { mapState, mapActions, mapMutations } from 'vuex';
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  },
   data() {
     return {
       trickName: '',
+      step: 1,
     }
   },
   computed: {
-    ...mapState({
-      message: state => state.message
-    }),
     ...mapState('tricks', {
       tricks: state => state.tricks
     }),
+    haveTricks() {
+      return this.tricks && this.tricks.length > 0;
+    }
   },
   methods: {
-    ...mapMutations(['reset']),
     ...mapMutations('tricks', {
       resetTricks: 'reset',
     }),
@@ -149,6 +96,19 @@ export default {
     async saveTrick() {
       await this.createTrick({ trick: { name: this.trickName }});
       this.trickName = '';
+    },
+    async handleFile(file) {
+      if (!file)
+        return;
+
+      console.log(file);
+
+      const formData = new FormData();
+      formData.append('video', file);
+
+      const result = await Axios.post('http://localhost:5000/api/videos', formData);
+
+      //this.$store.dispatch('uploadFile', this.$refs.file.files[0]);
     }
   },
 }
--- web-client/store/tricks.js
+++ web-client/store/tricks.js
@@ -1,5 +1,3 @@
-import Axios from 'axios';
-
 const initState = () => ({
   tricks: []
 });
@@ -17,11 +15,11 @@ export const mutations = {
 
 export const actions = {
   async fetchTricks({ commit }) {
-    const tricks = (await Axios.get('http://localhost:5000/api/tricks')).data;
+    const tricks = await this.$axios.$get('http://localhost:5000/api/tricks');
     commit('setTricks', { tricks });
   },
   async createTrick({ commit, dispatch }, { trick }) {
-    await Axios.post('http://localhost:5000/api/tricks', trick);
+    await await this.$axios.post('http://localhost:5000/api/tricks', trick);
     dispatch('fetchTricks');
   },
 };
